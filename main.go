9402659cfdd9bcffe999d32dc627e184705cdcef
safon <safonchik.jn@gmail.com>
2023-11-29 21:43:30 +0200

    single signature finished, but something doesn't work

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/sha256"
 	"fmt"
@@ -14,41 +15,66 @@ type KeyPair struct {
 }
 
 type Signature struct {
-	m string
 	R *big.Int
-	P *big.Int
-	e *big.Int
+	s *big.Int
 }
 
 func GenKeys() (keys KeyPair) {
 	g := elliptic_curves.BasePointGGet()
 	n := elliptic_curves.BasePointOrder()
-	keys.private, _ = rand.Int(rand.Reader, &n)                  //private key
-	keys.public = elliptic_curves.ScalarMult(*keys.private, g).X //public key
+	keys.private, _ = rand.Int(rand.Reader, &n) //private key
+	t := elliptic_curves.ScalarMult(*keys.private, g)
+	keys.public = t.X //public key
+	if keys.public.Cmp(new(big.Int)) == 0 || !elliptic_curves.IsOnCurveCheck(t) {
+		return GenKeys()
+	}
+	p := elliptic.P256().Params().P
+	gx := elliptic.P256().Params().Gx
+	gy := elliptic.P256().Params().Gy
+	x, y := elliptic.P256().ScalarBaseMult(p.Bytes())
+	if x.Cmp(gx) == 0 && y.Cmp(gy) == 0 {
+		fmt.Println("success")
+	}
+	if elliptic_curves.IsEqual(elliptic_curves.ScalarMult(*p, g), g) {
+		fmt.Println("aaa")
+	}
 	return keys
 }
 
-func GenNonce(k *big.Int) (nonceKeys KeyPair) {
+func SignMessage(m string, keys KeyPair) (signature Signature) {
 	g := elliptic_curves.BasePointGGet()
 	n := elliptic_curves.BasePointOrder()
-	nonceKeys.private, _ = rand.Int(rand.Reader, &n)       //private key
-	nonceKeys.public = elliptic_curves.ScalarMult(*k, g).X //public key
-	return nonceKeys
-}
-
-func SignMessage(m string, keys, nonceKeys KeyPair) (signature Signature) {
-	str := append(nonceKeys.public.Bytes(), keys.public.Bytes()...)
+	r, _ := rand.Int(rand.Reader, &n)                            //private key
+	signature.R = elliptic_curves.ScalarMult(*keys.private, g).X //public key
+	str := append(signature.R.Bytes(), keys.public.Bytes()...)
 	str = append(str, []byte(m)...)
 	hash := sha256.Sum256(str)
-	signature.e = new(big.Int).SetBytes(hash[:])
-	s := new(big.Int)
-	s.Add(nonceKeys.private, keys.private.Mul(keys.private, signature.e))
-	signature.m = m
-	signature.P = keys.public
-	signature.R = nonceKeys.public
+	e := new(big.Int).SetBytes(hash[:])
+	signature.s = new(big.Int)
+	var t big.Int
+	signature.s.Add(r, t.Mul(keys.private, e))
 	return signature
 }
 
+func Verify(signature Signature, m string, P *big.Int) (res bool) {
+	g := elliptic_curves.BasePointGGet()
+	str := append(signature.R.Bytes(), P.Bytes()...)
+	str = append(str, []byte(m)...)
+	hash := sha256.Sum256(str)
+	e := new(big.Int).SetBytes(hash[:])
+	sum := new(big.Int)
+	sG := elliptic_curves.ScalarMult(*signature.s, g).X
+	var t big.Int
+	sum.Add(sG, t.Mul(P, e))
+	fmt.Println(sum)
+	fmt.Println(signature.R)
+	return sum.Cmp(signature.R) == 0
+}
+
 func main() {
-	fmt.Println("")
+	m := "Hello, I am totally the person who signed this"
+	keys := GenKeys()
+	signature := SignMessage(m, keys)
+	res := Verify(signature, m, keys.public)
+	fmt.Println(res)
 }
